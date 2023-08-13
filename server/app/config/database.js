b85285d02874de95b99b140e7977be9c68052e6e
M Ibnu Umar R <fancycyanogen@gmail.com>
2023-08-13 20:57:47 +0700

    setting deploy

--- server/app/config/database.js
+++ server/app/config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/tokopedia_play"
+    // url: "mongodb://localhost:27017/tokopedia_play"
+    url: "mongodb+srv://fancycyanogen:adJSwuCNMOPY0PK8@gigih.dwm2yiq.mongodb.net/?retryWrites=true&w=majority"
 }
